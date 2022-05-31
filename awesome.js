fcb2cb0a3e264b8ab83980df42fda85e4443a4b7
Muhammad Shaheer <59507815+ShaheerCH@users.noreply.github.com>
2022-05-31 13:04:15 +0500

    Update awesome.js

--- awesome.js
+++ awesome.js
@@ -1,13 +1,13 @@
-// || =========== awesome ============ ||
-let arrBooks = JSON.parse(window.localStorage.getItem("arrBooksLocal"));
+ // || =========== awesome ============ ||
+let arrBooks = JSON.parse(window.localStorage.getItem('arrBooksLocal'));
 if (!arrBooks) {
   arrBooks = [
     {
       id: 1,
-      title: "The Adventures Of Sherlock Holmes",
-      author: "Sir Author Conan Doyle",
+      title: 'The Adventures Of Sherlock Holmes',
+      author: 'Sir Author Conan Doyle',
     },
-    { id: 2, title: "Around The World In 80 Days", author: "Jules Verne" },
+    { id: 2, title: 'Around The World In 80 Days', author: 'Jules Verne' },
   ];
 }
 
@@ -24,8 +24,8 @@ function deleteBook(bookID) {
 }
 
 function compileBookArticle(book) {
-  const articleBook = document.createElement("article");
-  articleBook.className = "books";
+  const articleBook = document.createElement('article');
+  articleBook.className = 'books';
   articleBook.id = `book-${book.id}`;
   articleBook.innerHTML = `<p>
   ${book.title}
@@ -40,30 +40,30 @@ function compileBookArticle(book) {
 
 function removeBooks(bookID) {
   // delete book from html booklist
-  bookID = bookID.replace(/\D/gi, "");
+  bookID = bookID.replace(/\D/gi, '');
   const article = document.getElementById(`book-${bookID}`);
   deleteBook(bookID);
-  window.localStorage.setItem("arrBooksLocal", JSON.stringify(arrBooks));
+  window.localStorage.setItem('arrBooksLocal', JSON.stringify(arrBooks));
   article.parentNode.removeChild(article);
 }
 
 function appendBooklist(book) {
   // append book to html booklist
-  const articleBooklist = document.getElementById("booklist");
-  articleBooklist.className = "booklist";
-  articleBooklist.id = "booklist";
+  const articleBooklist = document.getElementById('booklist');
+  articleBooklist.className = 'booklist';
+  articleBooklist.id = 'booklist';
   articleBooklist.appendChild(compileBookArticle(book));
-  document.getElementById(`id-${book.id}`).addEventListener("click", () => {
+  document.getElementById(`id-${book.id}`).addEventListener('click', () => {
     removeBooks(`id-${book.id}`);
   });
 }
 
-document.getElementById("add").addEventListener("click", (event) => {
-  const title = document.getElementById("title").value;
-  const author = document.getElementById("author").value;
+document.getElementById('add').addEventListener('click', (event) => {
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
   addBook(title, author);
   appendBooklist(arrBooks[arrBooks.length - 1]);
-  window.localStorage.setItem("arrBooksLocal", JSON.stringify(arrBooks));
+  window.localStorage.setItem('arrBooksLocal', JSON.stringify(arrBooks));
   event.stopPropagation();
 });
 
