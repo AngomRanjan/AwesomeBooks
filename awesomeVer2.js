f167fb038602a89626950f42d687696aea488924
Muhammad Shaheer <59507815+ShaheerCH@users.noreply.github.com>
2022-06-01 15:04:57 +0500

    Update awesomeVer2.js

--- awesomeVer2.js
+++ awesomeVer2.js
@@ -33,10 +33,10 @@ function Book(id, title, author) {
 const arrBooks = new ArrBooks();
 
 function saveLocal() {
-  localStorage.setItem("arrBooksLocal", JSON.stringify(arrBooks.books));
+  localStorage.setItem('arrBooksLocal', JSON.stringify(arrBooks.books));
 }
 
-const localBooks = JSON.parse(localStorage.getItem("arrBooksLocal"));
+const localBooks = JSON.parse(localStorage.getItem('arrBooksLocal'));
 
 if (localBooks) {
   arrBooks.books = localBooks;
@@ -44,28 +44,28 @@ if (localBooks) {
   arrBooks.books = [
     {
       id: 1,
-      title: "The Adventures Of Sherlock Holmes",
-      author: "Sir Author Conan Doyle",
+      title: 'The Adventures Of Sherlock Holmes',
+      author: 'Sir Author Conan Doyle',
     },
     {
       id: 2,
-      title: "Around The World In 80 Days",
-      author: "Jules Verne",
+      title: 'Around The World In 80 Days',
+      author: 'Jules Verne',
     },
     {
       id: 3,
-      title: "Great Expectation",
-      author: "Charles Dickens",
+      title: 'Great Expectation',
+      author: 'Charles Dickens',
     },
     {
       id: 4,
-      title: "The Three Musketeers",
-      author: "Alexander Dumas",
+      title: 'The Three Musketeers',
+      author: 'Alexander Dumas',
     },
     {
       id: 5,
-      title: "Robinson Crusoe",
-      author: "Daniel Defoe",
+      title: 'Robinson Crusoe',
+      author: 'Daniel Defoe',
     },
   ];
 }
@@ -73,11 +73,11 @@ if (localBooks) {
 // || ==== Compiling Book Card For Rendering In HTML Page (uses article element)=== ||
 
 function compileBookArticle(book) {
-  const articleBook = document.createElement("article");
-  articleBook.className = "books";
+  const articleBook = document.createElement('article');
+  articleBook.className = 'books';
   articleBook.id = `book-${book.id}`;
   articleBook.innerHTML = `<p>
-  "${book.title}"
+  '${book.title}'
   </p>
   <p>
   By ${book.author}
@@ -91,7 +91,7 @@ function compileBookArticle(book) {
 
 function removeBooks(bookID) {
   // remove book from html booklist, arrBooks and update locals
-  bookID = bookID.replace(/\D/gi, "");
+  bookID = bookID.replace(/\D/gi, '');
   const article = document.getElementById(`book-${bookID}`);
   arrBooks.deleteBook(bookID);
   saveLocal();
@@ -100,11 +100,11 @@ function removeBooks(bookID) {
 
 function appendBooklist(book) {
   // appending html booklist
-  const articleBooklist = document.getElementById("booklist");
-  articleBooklist.className = "booklist";
-  articleBooklist.id = "booklist";
+  const articleBooklist = document.getElementById('booklist');
+  articleBooklist.className = 'booklist';
+  articleBooklist.id = 'booklist';
   articleBooklist.appendChild(compileBookArticle(book));
-  document.getElementById(`id-${book.id}`).addEventListener("click", () => {
+  document.getElementById(`id-${book.id}`).addEventListener('click', () => {
     removeBooks(`id-${book.id}`);
   });
 }
@@ -114,21 +114,21 @@ for (let i = 0; i < arrBooks.books.length; i += 1) {
   appendBooklist(arrBooks.books[i]);
 }
 
-document.getElementById("add-book").addEventListener("submit", (event) => {
+document.getElementById('add-book').addEventListener('submit', (event) => {
   // Handling Form Add Button To Add Book, Append Html Booklist and Update local
   event.preventDefault();
   const newBookId = arrBooks.getNewID();
-  const title = document.getElementById("title").value;
-  const author = document.getElementById("author").value;
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
   arrBooks.addBook(new Book(newBookId, title, author));
   appendBooklist(arrBooks.getBookById(newBookId));
   saveLocal();
   event.stopPropagation();
 });
 
-const form = document.getElementById("add-book");
+const form = document.getElementById('add-book');
 
-form.addEventListener("submit", function handleSubmit(event) {
+form.addEventListener('submit', function handleSubmit(event) {
   event.preventDefault();
 
   form.reset();
