3a82ae3047395c9a3ebd60f0c5abbad54f0ca5c7
AngomRanjan <angomranjan@gmail.com>
2022-06-01 19:26:25 +0530

    Fix Linters

--- clock.js
+++ clock.js
@@ -1,24 +1,24 @@
 function showTime() {
-  var date = new Date();
-  var h = date.getHours(); // 0 - 23
-  var m = date.getMinutes(); // 0 - 59
-  var s = date.getSeconds(); // 0 - 59
-  var session = 'AM';
+  const date = new Date();
+  let h = date.getHours(); // 0 - 23
+  let m = date.getMinutes(); // 0 - 59
+  let s = date.getSeconds(); // 0 - 59
+  let session = 'AM';
 
-  if (h == 0) {
+  if (h === 0) {
     h = 12;
   }
 
   if (h > 12) {
-    h = h - 12;
+    h -= 12;
     session = 'PM';
   }
 
-  h = h < 10 ? '0' + h : h;
-  m = m < 10 ? '0' + m : m;
-  s = s < 10 ? '0' + s : s;
+  h = h < 10 ? `0${h}` : h;
+  m = m < 10 ? `0${m}` : m;
+  s = s < 10 ? `0${s}` : s;
 
-  var time = 'JUNE 1ST 2022, ' + h + ':' + m + ':' + s + ' ' + session;
+  const time = `JUNE 1ST 2022, ${h}:${m}:${s} ${session}`;
   document.getElementById('clock').innerText = time;
   document.getElementById('clock').textContent = time;
 
